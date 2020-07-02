7c3ad703cffc78f076d939fb1b7ac7018d0dcd79
Peter Kurtz <peter.ryan.kurtz@gmail.com>
2020-07-01 22:16:16 -0700

    Finished git setup script

--- arch_install/git_setup.sh
+++ arch_install/git_setup.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
 
+# Set this manually
+email=""
+name=""
+user=""
+pass=""
+
 ###
 # Use this script to set up git on a new machine(bash terminal)
 ###
 
-if pacman -Qs git > /dev/null; then
-    echo "git is installed"
-else
-    echo "git is no tinstalled"
-fi
+# To get this script git already has to be installed...
+
+mkdir ~/.config/git
+touch ~/.config/git/config
+
+echo "[user]" >> ~/.config/git/config
+echo -e "\temail=$email" >> ~/.config/git/config
+echo -e "\tname=$name" >> ~/.config/git/config
+
+touch ~/.config/git/credentials
+
+echo "https://$user:$pass@github.com" >> ~/.config/git/credentials
+
+git config credntial.helper store
